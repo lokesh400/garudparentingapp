27154fc09883bf9382a9006f8a2738270c42fabc
Lokesh <lokeshbadgujjar400@gmail.com>
2026-03-28 12:00:18 +0530

    asd

--- utils/api.js
+++ utils/api.js
@@ -1,14 +1,14 @@
 import axios from "axios";
 
-// export const API = axios.create({
-//   baseURL: "https://garudclasseserp.onrender.com",
-//   withCredentials: true,
-// });
-
 export const API = axios.create({
-  baseURL: "http://10.173.40.198:4000",
+  baseURL: "https://garudclasseserp.onrender.com",
   withCredentials: true,
-})
+});
+
+// export const API = axios.create({
+//   baseURL: "http://10.173.40.198:4000",
+//   withCredentials: true,
+// })
 
 
 
